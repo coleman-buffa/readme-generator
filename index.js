94c972327d2fae813bf64e050bc115a7fa27e399
Coleman Buffa <coleman.buffa@gmail.com>
2021-01-16 14:23:00 -0800

    Add init function and require additional imports.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
+const gMD = require('./utils/generateMarkdown.js');
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -10,7 +13,19 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    try {
+        const answers = await promptUser();
+
+        const md = gMD.generateMarkdown(answers);
+
+        writeFileAsync('blank-README.md', md);
+
+        console.log('Succesfully wrote to blank-README.md');
+    } catch(err) {
+        console.log(err);
+    }
+};
 
 // Function call to initialize app
 init();
